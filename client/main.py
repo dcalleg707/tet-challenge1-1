919de26a3719f654768ba62f091f7a7e3d3a6756
Juan Sebastián Díaz Osorio <jsdiazo@eafit.edu.co>
2021-09-28 15:28:50 -0500

    commands 'cd' and 'ls' added

--- client/main.py
+++ client/main.py
@@ -18,6 +18,61 @@ def print_title():
 
     print('Welcome to DistriB CLI!', end='\n\n')
 
+def command_checker(cmd, args):
+    # Manual
+    elif cmd == "man":
+        if args:
+            print(f'No manual entry for {args[0]}')
+        else:
+            print('What manual page do you want?\tTry \'man man\'')
+    
+    # Change Directory
+    elif cmd == "cd":
+        try:
+            if args:
+                if args[0] == "--help" or args[0] == '-h':
+                    print('cd: cd [dir]\n\tChange the shell working directory.')
+                else:
+                    directory = args if len(args) > 1 else args[0] 
+                    os.chdir(directory)
+
+        except FileNotFoundError as e:
+            print(f'cd: {args[0]}: No such file or directory')
+        except TypeError as e:
+            print('cd: too many arguments')
+    
+    # List files
+    elif cmd == "ls":
+        params = [s for s in args if s[0] == "-"]
+        no_params = [s for s in args if s[0] != "-"]
+        args = no_params if len(params) != len(args) else []
+        
+        hidden = False
+        if "-a" in params: hidden = True
+
+        if len(args) > 1:
+            for elem in args:
+                try:
+                    dirs = os.listdir(elem)
+                    print(f'{elem}:')
+                    for d in dirs:
+                        if d[0] != "." or hidden:
+                            print(d, end='  ')
+                    print('\n')
+                
+                except FileNotFoundError as e:
+                    print(f'ls: cannot access \'{elem}\': No such file or directory')
+        else:
+            dirs = os.listdir()
+            for d in dirs:
+                if d[0] != "." or hidden:
+                    print(d, end='  ')
+            print('')
+    
+    else:
+        print(f'{cmd}: command not found')
+
+
 def main():
     print('***********************************')
     user_input = ""
@@ -31,8 +86,12 @@ def main():
             command = user_input[0]
             args = user_input[1:] if len(user_input) > 1 else []
 
-            print(f'<<TEST STRING: COMMAND: {command}; args {args}>>')
-    
+            #print(f'<<TEST STRING: COMMAND: {command}; args {args}>>')
+
+            command_checker(command, args)
+            
+            
+
     except KeyboardInterrupt:
         print('\n\nBye!\n')
 
